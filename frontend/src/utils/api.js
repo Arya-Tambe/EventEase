5edd1ccf5cd68ba043422a3e706e59f61e0b4e5d
Arya-Tambe <arya.tambe5427@gmail.com>
2026-08-11 01:32:38 +0530

    Fix API variable case in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api'
+  baseURL: '/api'
 });
+
 // Add token to every request automatically
-API.interceptors.request.use((req) => {
+api.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
-export default API;
+export default api;

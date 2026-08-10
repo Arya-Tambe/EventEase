ecc0c14ccbaea3c1e717211c7118e655870cec43
Arya-Tambe <arya.tambe5427@gmail.com>
2026-08-10 23:02:31 +0530

    Clean old DevOps task implementation

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "===== EventEase Deploy ====="
-git pull origin main
-docker-compose pull
-docker-compose up -d --force-recreate --remove-orphans
-docker image prune -f
-docker-compose ps
-echo "===== Done ====="

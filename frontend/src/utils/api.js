58c1a4ddd644e0b8858ee9aeaea1620787010bf0
Arya-Tambe <arya.tambe5427@gmail.com>
2026-08-11 01:28:03 +0530

    Fix frontend API URL for EC2 deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+const api = axios.create({
+    baseURL: '/api'
 });
-
 // Add token to every request automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
